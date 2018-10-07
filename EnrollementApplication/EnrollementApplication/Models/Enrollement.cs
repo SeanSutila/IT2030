057390c5bd0cc565b2268a9698e271947b5c11fb
GodofBakon <ssutila@yahoo.com>
2018-10-07 15:27:27 -0400

    made Lab04 MVCMusicStore.  It didn't work still not creating DB.

--- EnrollementApplication/EnrollementApplication/Models/Enrollement.cs
+++ EnrollementApplication/EnrollementApplication/Models/Enrollement.cs
@@ -13,4 +13,5 @@ namespace EnrollementApplication.Models
         public virtual decimal Grade { get; set; }
         public virtual Student Student { get; set; }
         public virtual Course Course { get; set; }
+    }
 }
